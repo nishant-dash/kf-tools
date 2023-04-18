4f2a0d54948dfe505d3e70930e02ae0a6e896f84
nishant-dash <nishant.dash@canonical.com>
2023-04-18 22:04:00 +0400

    trying diff reqs format

--- setup.py
+++ setup.py
@@ -4,9 +4,12 @@ def readf(file):
     with open(file, "r", encoding="utf-8") as f:
         return f.read()
 
+with open("requirements.txt", "r", encoding="utf-8") as f:
+    reqs = f.read()
+
 setup(
     name = 'kft',
-    version = '0.1.2',
+    version = '0.1.3',
     author = 'Nishant Dash',
     author_email = 'nishant.dash@canonical.com',
     license = readf("LICENSE"),
@@ -15,7 +18,7 @@ setup(
     long_description_content_type = "text/markdown",
     url = 'https://github.com/nishant-dash/kf-tools',
     packages = find_packages(),
-    install_requires = [readf("requirements.txt")],
+    install_requires = [reqs],
     python_requires='>=3.7',
     entry_points = '''
         [console_scripts]
