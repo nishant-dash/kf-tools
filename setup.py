60911b1b4813bf41af686320f615421116a6ee2c
nishant-dash <nishant.dash@canonical.com>
2023-04-18 14:22:49 +0400

    update setup py

--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
 from setuptools import setup, find_packages 
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+with open("README.md", "r", encoding="utf-8") as f:
+    readme = f.read()
 
-with open("requirements.txt", "r", encoding="utf-8") as fh:
-    requirements = fh.read()
+with open("requirements.txt", "r", encoding="utf-8") as f:
+    requirements = f.read()
+
+with open("LICENSE", "r", encoding="utf-8") as f:
+    kft_license = f.read()
 
 setup(
     name = 'kft',
     version = '0.1',
     author = 'Nishant Dash',
     author_email = 'nishant.dash@canonical.com',
-    license = 'MIT',
+    license = kft_license,
     description = 'A collection of handy tools for operators of kubeflow environments',
-    long_description = long_description,
+    long_description = readme,
     long_description_content_type = "text/markdown",
     url = 'https://github.com/nishant-dash/kf-tools',
     py_modules = ['kft'],
